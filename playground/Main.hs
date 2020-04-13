c74bf93126e9b1acd7eeb36e451014c368731df8
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-04-13 01:35:32 -0700

    playground: add a stateful endpoint
    This lets us use the background like a REPL, with persistent mutable
    state.
    
    This commit introduces a new endpoint, POST /stateful, which takes a
    state parameter as an input (along with the expression to be evaluated)
    and returns an updated state parameter as an output (along with the
    result of evaluating the expression).

--- playground/Main.hs
+++ playground/Main.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass, DuplicateRecordFields, OverloadedStrings #-}
 module Main where
 
 import BasePrelude
+import Codec.Compression.Zstd.Lazy
+import Data.Aeson hiding (json)
+import qualified Data.ByteString.Base64.Lazy as Base64
 import Data.Text.Lazy
 import Data.Text.Lazy.Encoding
 import Web.Scotty
@@ -13,6 +16,22 @@ import Eval
 belDotBelState :: String
 belDotBelState = $(serializedBelDotBelState)
 
+data StatefulRequest = StatefulRequest
+  { expr :: String
+  , state :: Text
+  } deriving (Generic, FromJSON)
+
+data StatefulResponse = StatefulResponse
+  { result :: String
+  , state :: Text
+  } deriving (Generic, ToJSON)
+
+compressState :: String -> Text
+compressState = decodeUtf8 . Base64.encode . compress 1 . encodeUtf8 . pack
+
+decompressState :: Text -> String
+decompressState = unpack . decodeUtf8 . decompress . Base64.decodeLenient . encodeUtf8
+
 main :: IO ()
 main = stringToState belDotBelState >>= either die pure >>= withNativeFns >>= \baseState ->
   (read . fromMaybe "8080" <$> lookupEnv "PORT") >>= flip scotty do
@@ -22,4 +41,18 @@ main = stringToState belDotBelState >>= either die pure >>= withNativeFns >>= \b
       body >>= liftIO . flip (readThenRunEval "") baseState . unpack . decodeUtf8
         >>= either pure repr . fst >>= text . pack
 
-    get "/" $ text $ pack "Chime API Server - send me Bel code and get eval results!"
+    post "/stateful" do
+      addHeader "Access-Control-Allow-Origin" "*"
+      body >>= (. decode) \case
+        Nothing -> text "malformed request"
+        Just req -> do
+          hydrated <- case state (req :: StatefulRequest) of
+            "" -> pure baseState
+            s -> stringToState (decompressState s) >>=
+              either ((*> finish) . text . pack . ("malformed state: " <>)) withNativeFns
+          (r, s) <- liftIO $ readThenRunEval "" (expr req) hydrated
+          r' <- either pure repr r
+          s' <- compressState <$> stateToString s
+          json $ StatefulResponse {result=r', state=s'}
+
+    get "/" $ text "Chime API Server - send me Bel code and get eval results!"
